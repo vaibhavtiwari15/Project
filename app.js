344439cc6ffc5de28df7ad7def5177202216e95b
vaibhavtiwari15 <148370105+vaibhavtiwari15@users.noreply.github.com>
2023-10-18 22:37:44 +0530

    today i am writing my js program

---
+++ app.js
@@ -0,0 +1,11 @@
+function findLargestNumber(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+console.log(findLargestNumber(123, 456, 789)); // Output: 789
